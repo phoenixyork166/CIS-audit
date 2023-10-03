c042178cb2599f89fdc5b80e0e3e72e98a2e0f8f
phoenix166 <berlinlee2017@gmail.com>
2023-10-03 08:20:39 -0400

    verify grub.cfg

--- linux-hardening/verifyBootGrub.py
+++ linux-hardening/verifyBootGrub.py
@@ -12,87 +12,78 @@ def verifyBootGrub():
     user = input(Fore.WHITE + "Running this script as: ")    
     sudo_password = getpass.getpass(prompt='Enter sudo password: ')
     print(Fore.WHITE + "Changing /boot/grub/grub.cfg permissions...\n")
-    grubPath = '';
-    grubConf = '';
-    permission = '-rw------- 1 root root'
-    hardenedValue = re.search("(-rw-------)\s+\d+\s+(root)\s+(root)", permission)
-    print(hardenedValue)
-    os = input(Fore.YELLOW + "\nEnter your Linux OS version [Suse15/Suse12/Suse11/Redhat/Debian/Kali]: ")
     
-    if os.lower() == "suse15" or "suse12" or "redhat":
-        grubPath = '/boot/grub/'
-        grubConfPath = '/boot/grub/grub.cfg'
-        grubConfName = 'grub.cfg'
-        
+    grubPath = '/boot/grub/'
+    grubConfPath = '/boot/grub/grub.cfg'
+    grubCfgName = 'grub.cfg'
+
+    permission = '-rw------- 1 root root'
+    #hardenedValue = re.search("(-rw-------)\s+\d+\s+(root)\s+(root)", permission)
+    print(Fore.YELLOW + "\nCurrent /boot/grub/grub.cfg permission: \n")
+    currentCfgPermission = os.system('ls -la /boot/grub/ | grep grub.cfg | awk \'{{print $1,$2,$3,$4}}\''.format(user))
+    currentCfgPermission
+    print(Fore.YELLOW + "====================================")
+    ver = input(Fore.YELLOW + "\nEnter your Linux OS version [Suse15/Suse12/Suse11/Redhat/Debian/Kali]: ")
+    
+    if ver.lower() == str('kali'):       
         ##
         changeGrubConfPermission = f'echo {sudo_password} | sudo chmod 600 {grubConfPath}'
         doChangeGrubConfPermission = subprocess.Popen(changeGrubConfPermission, shell=True, text=True)
         doChangeGrubConfPermission.wait()
         if doChangeGrubConfPermission.returncode == 0:
             print(Fore.YELLOW + "\nChanging grub.cfg Permission has succeeded!\n")
-            print(Fore.YELLOW + "\nVerifying grub.cfg Permission...\n")
-            awk = "awk '{print $1,$2,$3,$4}'"
-            verifyGrubConfPermission = f"echo {sudo_password} | sudo ls -la {grubPath} | grep {grubConfName} | awk '{print \$1,\$2,\$3,\$4}'"
-            doVerifyGrubConfPermission = subprocess.Popen(verifyGrubConfPermission, shell=True, text=True, stdout=subprocess.PIPE)
-            doVerifyGrubConfPermission.wait()
-            if doVerifyGrubConfPermission.returncode == 0:
-                print(doVerifyGrubConfPermission.stdout)
-                ##
-                            
-            else:
-                print(Fore.RED + "\nFailed to verify grub.cfg permissions...\nSkipping...\n")
+            print(Fore.YELLOW + "\nVerifying the new grub.cfg Permission...\n")
+
+            #verifyGrubConfPermission = f"echo {sudo_password} | sudo ls -la {grubPath} | grep {grubCfgName} | awk '{{print $1,$2,$3,$4}}'"
+            #doVerifyGrubConfPermission = subprocess.Popen(verifyGrubConfPermission, shell=True)
+            #doVerifyGrubConfPermission.wait()
+            newPermission = os.system('ls -la /boot/grub/ | grep grub.cfg | awk \'{{print $1,$2,$3,$4}}\''.format(user))
+            newPermission
+            print(Fore.YELLOW + "\nHardening for /boot/grub/grub.cfg has succeeded!\n")
         else:
             print(Fore.RED + "\nFailed to change grub.cfg Permissions...\nSkipping...\n")
         ##
 
-    elif os.lower() == "suse11":
-        grubPath = '/boot/grub/'
-        grubConfPath = '/boot/grub/menu.lst'
-        grubConfName = 'menu.lst'
-        ##
-        changeGrubConfPermission = f'echo {sudo_password} | sudo chmod 600 {grubConfPath}'
-        doChangeGrubConfPermission = subprocess.Popen(changeGrubConfPermission, shell=True, text=True)
-        doChangeGrubConfPermission.wait()
-        if doChangeGrubConfPermission.returncode == 0:
-            print(Fore.YELLOW + "\nChanging grub.cfg Permission has succeeded!\n")
-            print(Fore.YELLOW + "\nVerifying grub.cfg Permission...\n")
-            awk = "awk '{print $1,$2,$3,$4}'"
-            verifyGrubConfPermission = f"echo {sudo_password} | sudo ls -la {grubPath} | grep {grubConfName} | awk '{print \$1,\$2,\$3,\$4}'"
-            doVerifyGrubConfPermission = subprocess.Popen(verifyGrubConfPermission, shell=True, text=True, stdout=subprocess.PIPE)
-            doVerifyGrubConfPermission.wait()
-            if doVerifyGrubConfPermission.returncode == 0:
-                print(doVerifyGrubConfPermission.stdout)
-                ##
-                            
-            else:
-                print(Fore.RED + "\nFailed to verify grub.cfg permissions...\nSkipping...\n")
-        else:
-            print(Fore.RED + "\nFailed to change grub.cfg Permissions...\nSkipping...\n")
-        ##
+    # elif ver.lower() == str('suse11'):
+    #     grubPath = '/boot/grub/'
+    #     grubConfPath = '/boot/grub/menu.lst'
+    #     grubCfgName = 'menu.lst'
+    #     ##
+    #     changeGrubConfPermission = f'echo {sudo_password} | sudo chmod 600 {grubConfPath}'
+    #     doChangeGrubConfPermission = subprocess.Popen(changeGrubConfPermission, shell=True, text=True)
+    #     doChangeGrubConfPermission.wait()
+    #     if doChangeGrubConfPermission.returncode == 0:
+    #         print(Fore.YELLOW + "\nChanging grub.cfg Permission has succeeded!\n")
+    #         print(Fore.YELLOW + "\nVerifying grub.cfg Permission...\n")
 
-    elif os.lower() == "kali":
-        grubPath = '/boot/grub/'
-        grubConfPath = '/boot/grub/grub.cfg'
-        grubConfName = 'grub.cfg'
-        ##
-        changeGrubConfPermission = f'echo {sudo_password} | sudo chmod 600 {grubConfPath}'
-        doChangeGrubConfPermission = subprocess.Popen(changeGrubConfPermission, shell=True, text=True)
-        doChangeGrubConfPermission.wait()
-        if doChangeGrubConfPermission.returncode == 0:
-            print(Fore.YELLOW + "\nChanging grub.cfg Permission has succeeded!\n")
-            print(Fore.YELLOW + "\nVerifying grub.cfg Permission...\n")
-            awk = "awk '{print $1,$2,$3,$4}'"
-            verifyGrubConfPermission = f"echo {sudo_password} | sudo ls -la {grubPath} | grep {grubConfName} | awk '{print \$1,\$2,\$3,\$4}'"
-            doVerifyGrubConfPermission = subprocess.Popen(verifyGrubConfPermission, shell=True, text=True, stdout=subprocess.PIPE)
-            doVerifyGrubConfPermission.wait()
-            if doVerifyGrubConfPermission.returncode == 0:
-                print(doVerifyGrubConfPermission.stdout)
-                ##
-                           
-            else:
-                print(Fore.RED + "\nFailed to verify grub.cfg permissions...\nSkipping...\n")
-        else:
-            print(Fore.RED + "\nFailed to change grub.cfg Permissions...\nSkipping...\n")
+    #         verifyGrubConfPermission = f"echo {sudo_password} | sudo ls -la {grubPath} | grep {grubCfgName} | awk '{{print $1,$2,$3,$4}}'"
+    #         doVerifyGrubConfPermission = subprocess.Popen(verifyGrubConfPermission, shell=True, text=True, stdout=subprocess.PIPE)
+    #         doVerifyGrubConfPermission.wait()
+    #         os.system('sudo -u {} sudo ls -la {grubPath} | grep {grubCfgName} | awk '{print $1,$2,$3,$4}''.format(user))
+    #         ##
+    #     else:
+    #         print(Fore.RED + "\nFailed to change grub.cfg Permissions...\nSkipping...\n")
+    #     ##
+
+    # elif ver.lower() == str('suse15') or str('suse12') or str('redhat'):
+    #     grubPath = '/boot/grub2/'
+    #     grubConfPath = '/boot/grub2/grub.cfg'
+    #     grubCfgName = 'grub.cfg'
+    #     ##
+    #     changeGrubConfPermission = f'echo {sudo_password} | sudo chmod 600 {grubConfPath}'
+    #     doChangeGrubConfPermission = subprocess.Popen(changeGrubConfPermission, shell=True, text=True)
+    #     doChangeGrubConfPermission.wait()
+    #     if doChangeGrubConfPermission.returncode == 0:
+    #         print(Fore.YELLOW + "\nChanging grub.cfg Permission has succeeded!\n")
+    #         print(Fore.YELLOW + "\nVerifying grub.cfg Permission...\n")
+
+    #         verifyGrubConfPermission = f"echo {sudo_password} | sudo ls -la {grubPath} | grep {grubCfgName} | awk '{{print $1,$2,$3,$4}}'"
+    #         doVerifyGrubConfPermission = subprocess.Popen(verifyGrubConfPermission, shell=True, text=True, stdout=subprocess.PIPE)
+    #         doVerifyGrubConfPermission.wait()
+    #         os.system('sudo -u {} sudo ls -la {grubPath} | grep {grubCfgName} | awk '{print $1,$2,$3,$4}''.format(user))
+    #         ##
+    #     else:
+    #         print(Fore.RED + "\nFailed to change grub.cfg Permissions...\nSkipping...\n")
         ##
 
     else: 
